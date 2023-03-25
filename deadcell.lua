8ab85276d491ed1fe8f33753d228478b40b8ad0c
p1nnacle <122668966+pinnacular@users.noreply.github.com>
2023-03-25 10:47:12 +0800

    Update deadcell.lua

--- deadcell.lua
+++ deadcell.lua
@@ -4,7 +4,7 @@ local settings = {
     default_accent = Color3.fromRGB(61, 100, 227); -- also stupid shit
 };
 
-local drawing = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tamim468/releases/main/extension.lua"))();
+local drawing = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tamim468/releases/main/extension.lua"))()
 local tween = loadstring(game:HttpGet("https://raw.githubusercontent.com/vozoid/utility/main/Tween.lua"))()
 
 -- // UI LIBRARY
@@ -181,7 +181,7 @@ library.gradient = images.gradient90 --decode("iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKC
 library.utility = utility
 
 function utility.outline(obj, color)
-    local outline = Drawing.new("Square")
+    local outline = drawing:new("Square")
     outline.Parent = obj
     outline.Size = UDim2.new(1, 2, 1, 2)
     outline.Position = UDim2.new(0, -1, 0, -1)
@@ -202,7 +202,7 @@ function utility.outline(obj, color)
 end
 
 function utility.create(class, properties)
-    local obj = Drawing.new(class)
+    local obj = drawing:new(class)
 
     for prop, v in next, properties do
         if prop == "Theme" then
